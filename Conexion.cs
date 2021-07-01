b24bfcd412a689a4b6ee07112f0cb4384cc5c723
guaaan <juampablo.txt@gmail.com>
2021-07-01 16:51:54 -0400

    Agregar archivos de proyecto.

---
+++ Conexion.cs
@@ -0,0 +1,35 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDF
+{
+    class Conexion
+    {
+        public static MySqlConnection conexion()
+        {
+            string servidor = "localhost";
+            string bd = "productos_db";
+            string usuario = "root";
+            string password = "FG12345";
+
+            string cadenaConexion = "Database=" + bd + "; Data Source=" + servidor + ";User id= " + usuario + ";Password=" + password + "";
+
+            try
+            {
+                MySqlConnection conexionBD = new MySqlConnection(cadenaConexion);
+                return conexionBD;
+            }
+            catch(MySqlException ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+                return null;
+            }
+
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,53 @@
+﻿using iText.IO.Font.Constants;
+using iText.Kernel.Font;
+using iText.Kernel.Geom;
+using iText.Kernel.Pdf;
+using iText.Layout;
+using iText.Layout.Element;
+using iText.Layout.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PDF
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGenerar_Click(object sender, EventArgs e)
+        {
+            crearPDF();
+        }
+        private void crearPDF()
+        {
+            PdfWriter pdfWriter = new PdfWriter("Reporte.pdf");
+            PdfDocument pdf = new PdfDocument(pdfWriter);
+            Document documento = new Document(pdf, PageSize.LETTER);
+
+            documento.SetMargins(60, 20, 55, 20);
+
+            PdfFont fontColumnas = PdfFontFactory.CreateFont(StandardFonts.HELVETICA_BOLD);
+            PdfFont fontContenido = PdfFontFactory.CreateFont(StandardFonts.HELVETICA);
+
+            string[] columnas = { "idArticulo", "Nombre", "Precio" };
+            float[] tamanios = { 2, 4, 2, 2, 4 };
+            Table tabla = new Table(UnitValue.CreatePercentArray(tamanios));
+            tabla.SetWidth(UnitValue.CreatePercentValue(100));
+
+
+
+
+            documento.Close();
+        }
+    }
+}
